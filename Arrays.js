19eba152438100dd61f84bb19edb4adc45a1d9a4
Emanuel Joness <itzemanuelj@gmail.com>
2022-09-02 21:56:20 -0400

    1-10 of arrays

---
+++ Arrays.js
@@ -0,0 +1,44 @@
+// What is a variable that represents a list of values? Answer with one word.
+
+Array
+
+// An array comprises any number of ______. ("Values" would be correct, but I'm looking for another word.)
+
+Element
+
+
+// The number inside the square brackets is the element's ____.
+
+Index
+
+// In an array, the numbering starts with __.
+
+0
+
+// Create a new array, nums, comprising three numbers from 3 to 5.
+
+var nums = [3,4,5];
+
+// This array has been defined.
+
+var names = ["Alf", "Bo", "Gail"];
+
+// Specifying an array element, code an alert that displays "Gail".
+
+alert(names[2]);
+
+// Create an array with a single element, a string. The array hasn't been declared beforehand.
+
+var elem = ["tom"];
+
+// Create an array with three elements that are numbers. It hasn't been declared beforehand.
+
+var numArray = [1, 2, 3];
+
+// Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+
+var elementArray = [cost];
+
+// Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+
+var elementsArray = [1, "hello", cost];
