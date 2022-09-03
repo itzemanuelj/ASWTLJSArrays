61d45f2f66e9b268d9ca7336d5ba0a52bbe996d8
Emanuel Joness <itzemanuelj@gmail.com>
2022-09-03 13:23:17 -0400

    mvp commmit for arrays

--- Arrays.js
+++ Arrays.js
@@ -42,3 +42,51 @@ var elementArray = [cost];
 // Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
 
 var elementsArray = [1, "hello", cost];
+
+// Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+
+var arraystr = ["hello","world"];
+
+var arrayReassign = arraystr[2];
+
+// This array has been defined.
+var names = ["Alf", "Bo", "Gail"];
+// Specifying an array element, code an alert that displays "Gail".
+
+alert(names[2]);
+
+// Create an array with one string element. The array hasn't been declared beforehand.
+
+ var oneArray = ["hello"];
+
+// Create an array with three elements that are numbers. It hasn't been declared beforehand.
+
+var threeArray = [1,2,3];
+
+// Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+
+var firstElement = name[0];
+
+// Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+
+var threeElems =  [1, "hello world", cost];
+
+// Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+
+var twoStringArray = ["one", "two",];
+
+var newArray = cost[2];
+
+// Declare an array with three elements.
+// Display the last element in an alert.
+// Click the Run It button to run your code live.
+
+var array3 = ["hello", "world", "!"];
+alert(array3[2]);
+
+// Declare an array with four elements.
+// Display the first element in an alert.
+// Click the Run It button to run your code live.
+
+var fourArray = [1,2,3,4];
+alert(fourArray[0]);
